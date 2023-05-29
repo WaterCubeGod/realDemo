035a94b3726910647c6e50947c852c64eb05830b
WaterCube <3166474860@qq.com>
2023-05-29 22:54:37 +0800

    Initial commit

---
+++ src/main/java/com/neu/wb/realdemo/RealDemoApplication.java
@@ -0,0 +1,13 @@
+package com.neu.wb.realdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealDemoApplication.class, args);
+    }
+
+}
