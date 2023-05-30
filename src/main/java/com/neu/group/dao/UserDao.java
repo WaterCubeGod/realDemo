4a91e00ef94e80d3d9a3d788d7964b6ef68355e0
xiongzixiang <xzx20030915@gmail.com>
2023-05-30 23:16:36 +0800

    修复了Mapper接口的问题

--- src/main/java/com/neu/group/dao/UserDao.java
+++ src/main/java/com/neu/group/dao/UserDao.java
@@ -13,20 +13,16 @@ import java.util.List;
 public interface UserDao {
 
     //通过账号id和密码来查询某一行
-    @Select("SELECT id,password,username,type FROM user WHERE id = ${id} AND password = ${password}")
     User selectByUsernameAndPassword(@Param("id") int id,@Param("password") String password);
 
     //插入一行
-    @Insert("INSERT INTO user(username,password,type) VALUES (${username},${password}},${type})")
     int insertUser(@Param("username") String username, @Param("password") String password,
                    @Param("type") int type);
 
     //删除一行:必须知道对应的id和密码
-    @Delete("DELETE FROM user WHERE id = ${id} AND password = ${password}")
     int deleteUser(@Param("id") int id,@Param("password") String password);
 
     //查找10行数据
-    @Select("SELECT id,username,type FROM user WHERE id > ${count} limit 10")
     List<User> selectAllUser(int count);
 
 }
